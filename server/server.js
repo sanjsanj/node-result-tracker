fc1ae7263aa610a623d71f7bef11eadb704326e6
Sanjay Purswani <sanjsanj@hotmail.com>
2018-10-24 15:53:10 +0100

    Create a barebones front end

--- server/server.js
+++ server/server.js
@@ -5,14 +5,18 @@ const _ = require("lodash");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { ObjectID } = require("mongodb");
+const path = require("path");
 
 const { User } = require("./models/user");
 const { Result } = require("./models/result");
 const { authenticate } = require("./middleware/authenticate");
 
+const publicPath = path.join(__dirname, "../public");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(publicPath));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
